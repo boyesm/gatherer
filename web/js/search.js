78487a65d72790bfab87df9bd4638e5e6b85eb8d
Malcolm Boyes <38740057+boyesm@users.noreply.github.com>
2021-11-21 00:56:19 -0500

    v1!

--- web/js/search.js
+++ web/js/search.js
@@ -3,28 +3,41 @@ function isValidDomain(domain) {
     return pattern.test(domain)
 }
 
-function displayUserMessage(message){
+function displayUserMessage(message) {
     document.getElementById('user-message').innerHTML = message
     document.getElementById('user-message').style.display = "block"
 }
 
-function hideUserMessage(){
+function hideUserMessage() {
     document.getElementById('user-message').style.display = "none"
 }
 
-function newSearchResultLine(text, number){
+function newSearchResultLine(text, number) {
     return `<li class="list-group-item d-flex justify-content-between align-items-center">
             ${text}
             <span class="badge bg-primary rounded-pill">${number}</span>
     </li>`
 }
 
+function searchLoadingOn() {
+    document.getElementById('domain-search-button').innerHTML = `
+    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    Loading...
+    `
+}
+
+function searchLoadingOff() {
+    document.getElementById('domain-search-button').innerHTML = `Search`
+}
 
 async function search() {
+    searchLoadingOn()
+
     // get domain from textbox
     let domain = document.getElementById('domain-search-box').value
 
-    let req = await fetch(`/.netlify/functions/test?domain=${domain}`)
+    // TODO: if this takes longer than n seconds, display an error
+    let req = await fetch(`/.netlify/functions/process?domain=${domain}`)
 
     // get data from database
     // end point will return either an error or a list of domains
@@ -34,10 +47,11 @@ async function search() {
     let rt = await req.text()
     let obj = await JSON.parse(rt)
 
-    if(req.status != 200){
+    if (req.status != 200) {
 
         // display message from
         displayUserMessage(`Error: ${obj.message}`)
+        searchLoadingOff()
         return
 
     } else {
@@ -46,21 +60,21 @@ async function search() {
 
         // display all enteries
         output = ""
-        obj.results.forEach(
+        // console.log(rt)
+        await obj.results.forEach(
             element => {
-                output += newSearchResultLine(element.email, element.n_appearences)
+                output += newSearchResultLine(element.email, element.n_appearances)
             }
         )
         document.getElementById('search-results').innerHTML = output
 
+        searchLoadingOff()
     }
 
 
-
-
-        // .then(r => r.text())
-        // .then(rt => JSON.parse(rt))
-        // .then(obj => displayUserMessage(obj.domain))
+    // .then(r => r.text())
+    // .then(rt => JSON.parse(rt))
+    // .then(obj => displayUserMessage(obj.domain))
 
     // // basic validitation
     // // const isValidDomain = require("is-valid-domain")  // use this on the endpoint to validate domain before searching!!!
